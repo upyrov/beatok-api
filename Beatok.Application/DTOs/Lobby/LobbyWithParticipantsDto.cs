5c1c4f3672f599073beda19f777970972ff92fa5
flamylight <y.dmytrenko11@gmail.com>
2026-07-23 17:19:43 +0300

    refactor: add LobbyPhase to LobbyWithParticipantsDto

--- Beatok.Application/DTOs/Lobby/LobbyWithParticipantsDto.cs
+++ Beatok.Application/DTOs/Lobby/LobbyWithParticipantsDto.cs
@@ -1,5 +1,6 @@
 ﻿using Beatok.Application.DTOs.Genre;
 using Beatok.Application.DTOs.User;
+using Beatok.Domain.Entities;
 
 namespace Beatok.Application.DTOs.Lobby;
 
@@ -10,6 +11,7 @@ public record LobbyWithParticipantsDto
     public required GenreDto Genre { get; set; }
     public required UserDto Owner { get; set; }
     public int ParticipantLimit { get; set; }
+    public LobbyPhase Phase { get; set; } 
     public ICollection<UserDto> Participants { get; set; } = [];
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public TimeSpan SubmissionTimeLimit { get; set; }
