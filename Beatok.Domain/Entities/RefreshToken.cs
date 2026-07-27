65ce1df6754baa8addfe235121693eefa66dbee5
upxrov <upyrov@outlook.com>
2026-07-27 20:40:00 +0300

    feat: make refresh token hash nullable

--- Beatok.Domain/Entities/RefreshToken.cs
+++ Beatok.Domain/Entities/RefreshToken.cs
@@ -3,7 +3,7 @@ namespace Beatok.Domain.Entities;
 public class RefreshToken
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public required string TokenHash { get; set; }
+    public string? TokenHash { get; set; }
     public Guid UserId { get; set; }
     public User? User { get; set; }
     public DateTime Expires { get; set; }
