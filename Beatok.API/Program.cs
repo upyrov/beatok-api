359861a501e559ac7ce43540b226645556fc31b6
upxrov <upyrov@outlook.com>
2026-08-01 13:57:41 +0300

    chore: add frontend url environment variable

--- Beatok.API/Program.cs
+++ Beatok.API/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("https://localhost:5173")
+            .WithOrigins(builder.Configuration.GetValue<string>("FrontendUrl")!)
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
