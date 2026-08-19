c9543440453fb9ccf6627454b88a578f9bdb4c1a
upxrov <upyrov@outlook.com>
2026-08-19 16:35:09 +0300

    fix: add picture to leaderboard user

--- Beatok.Application/DTOs/User/LeaderboardUserDto.cs
+++ Beatok.Application/DTOs/User/LeaderboardUserDto.cs
@@ -1,10 +1,6 @@
 namespace Beatok.Application.DTOs.User;
 
-public record LeaderboardUserDto
+public record LeaderboardUserDto : UserDto
 {
-    public required string Id { get; set; }
-    public required string Name { get; set; }
-    public string? Picture { get; set; }
-    public int Rating { get; set; }
     public int Wins { get; set; }
 }
