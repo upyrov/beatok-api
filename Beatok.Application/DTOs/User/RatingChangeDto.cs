bdb23bb9767c9354e9569804e447f7514df4a394
flamylight <y.dmytrenko11@gmail.com>
2026-08-21 13:14:48 +0300

    fix: serialize UserId in rating changes

--- Beatok.Application/DTOs/User/RatingChangeDto.cs
+++ Beatok.Application/DTOs/User/RatingChangeDto.cs
@@ -2,6 +2,6 @@ namespace Beatok.Application.DTOs.User;
 
 public record RatingChangeDto
 {
-    public required string UserId;
+    public required string UserId { get; set; }
     public double RatingChange { get; set; }
 };
